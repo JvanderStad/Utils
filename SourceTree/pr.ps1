dc70c9efab1fe10298e2b7e786280389ae7f500e
Jeffrey van der Stad <jeffrey@hybridsaas.com>
2017-05-19 15:34:54 +0200

    Update pr.ps1

--- SourceTree/pr.ps1
+++ SourceTree/pr.ps1
@@ -1,6 +1,8 @@
-Add-Type -AssemblyName System.Web;
-[void] ($remote = &git remote get-url origin);
-[void] ($branchName = &git rev-parse --abbrev-ref HEAD);
+Add-Type -AssemblyName System.Web
 
-$url = "$($remote)/pullrequestcreate?sourceRef=$([System.Web.HttpUtility]::UrlEncode($branchName) )&targetRef=master";
+[void] ($remote = &git remote get-url origin)
+[void] ($branchName = &git rev-parse --abbrev-ref HEAD)
+
+$url = "$($remote)/pullrequestcreate?sourceRef=$([System.Web.HttpUtility]::UrlEncode($branchName) )&targetRef=master"
+
 Start-Process "chrome.exe" $url
